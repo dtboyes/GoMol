d91d98f97a9718a2561cca78158e2f0936e2737c
Minhyek Jeon <minhyek0326@gmail.com>
2023-12-03 05:53:21 -0500

    12_3_5:53am

--- GoMol/app.py
+++ GoMol/app.py
@@ -4,7 +4,8 @@ import subprocess
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = r"C:\Users\BLUE OCEAN\Desktop\FlaskWeb\GoMol-main\GoMol-main"
+#change your path based on the location of your file
+UPLOAD_FOLDER = r"C:\Users\BLUE OCEAN\Desktop\FlaskWeb\GoMol-main\GoMol-main\GoMol"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -47,12 +48,41 @@ def result2():
 
     # Execute the GoMol program (assuming data is formatted as expected)
     execute_gomol(*data.split('\n')[0:3])  # assuming data has PDB ID1, PDB ID2, Render Chain A in the first 3 lines
+    line1, line2, line3 = display_sequence()
 
-    return render_template('result2.html')
+    return render_template('result2.html', seq1= line1, match=line2, seq2=line3)
 
 def execute_gomol(pdb_id_1, pdb_id_2, render_chain_a):
     # Execute the GoMol program and pass the user data
-    subprocess.run([r"C:\Users\BLUE OCEAN\Desktop\FlaskWeb\GoMol-main\GoMol-main\GoMol\GoMol.exe", pdb_id_1, pdb_id_2, render_chain_a])
+    path = os.path.join(UPLOAD_FOLDER, "GoMol.exe")
+    subprocess.run([path, pdb_id_1, pdb_id_2, render_chain_a])
+
+
+def display_sequence():
+    # sequence.txt 파일 읽기
+    file_path = 'result.txt'
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    line1 = lines[0]
+    line2 = lines[1]
+    line3 = lines[2]
+
+
+
+    return line1, line2, line3
+
+
+def read_lines(filename):
+    # 파일 경로
+    file_path = os.path.join(os.path.dirname(__file__), filename)
+    
+    # 파일 읽기
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    return lines
 
 if __name__ == "__main__":
     app.run(debug=True)
